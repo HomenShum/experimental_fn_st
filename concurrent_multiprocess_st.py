15a6307c42d9875afb55249348963e2e741d8892
homen <hshum2018@gmail.com>
2024-01-13 14:16:13 -0800

    check7

--- concurrent_multiprocess_st.py
+++ concurrent_multiprocess_st.py
@@ -17,32 +17,65 @@ def task(v):
 
 
 if __name__ == '__main__':
-    jobs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    jobs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     num_workers = len(multiprocessing.cpu_count())
     processed_jobs = []
 
-    start = st.button('start work')
+    # 2 col
+    col1, col2 = st.columns(2)
 
-    if start:
-        start_time = time.time()
-        with ProcessPoolExecutor(max_workers=num_workers) as executor:
-            for j in jobs:
-                pj = executor.submit(task, j)
-                processed_jobs.append(pj)
+    with col1:
 
-            for future in concurrent.futures.as_completed(processed_jobs):
-                try:
-                    res = future.result()
-                    # st.write(f'res: {res}')
+        start16 = st.button('start work 16 cpus')
 
-                    # Incrementally save the completed task so far.
-                    st.session_state.save.append(res)
+        if start16:
+            start_time = time.time()
+            with ProcessPoolExecutor(max_workers=num_workers) as executor:
+                for j in jobs:
+                    pj = executor.submit(task, j)
+                    processed_jobs.append(pj)
 
-                except concurrent.futures.process.BrokenProcessPool as ex:
-                    raise Exception(ex)
+                for future in concurrent.futures.as_completed(processed_jobs):
+                    try:
+                        res = future.result()
+                        # st.write(f'res: {res}')
 
-    if len(st.session_state.save):
-        st.write(multiprocessing.cpu_count())
-        st.success(f'Completed in {time.time() - start_time} seconds')
-        st.write('#### Completed Jobs')
-        st.write(f'{st.session_state.save}')
+                        # Incrementally save the completed task so far.
+                        st.session_state.save.append(res)
+
+                    except concurrent.futures.process.BrokenProcessPool as ex:
+                        raise Exception(ex)
+
+        if len(st.session_state.save):
+            st.write(multiprocessing.cpu_count())
+            st.success(f'Completed in {time.time() - start_time} seconds')
+            st.write('#### Completed Jobs')
+            st.write(f'{st.session_state.save[-1]}')
+
+    with col2:
+
+        start8 = st.button('start work 8 cpus')
+
+        if start8:
+            start_time = time.time()
+            with ProcessPoolExecutor(max_workers=8) as executor:
+                for j in jobs:
+                    pj = executor.submit(task, j)
+                    processed_jobs.append(pj)
+
+                for future in concurrent.futures.as_completed(processed_jobs):
+                    try:
+                        res = future.result()
+                        # st.write(f'res: {res}')
+
+                        # Incrementally save the completed task so far.
+                        st.session_state.save.append(res)
+
+                    except concurrent.futures.process.BrokenProcessPool as ex:
+                        raise Exception(ex)
+
+        if len(st.session_state.save):
+            st.write(multiprocessing.cpu_count())
+            st.success(f'Completed in {time.time() - start_time} seconds')
+            st.write('#### Completed Jobs')
+            st.write(f'{st.session_state.save[-1]}')
